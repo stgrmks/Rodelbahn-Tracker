0af07881e01a7d6d928ff28a6f7f0a12773d4734
stgrmks <m.steger@pm.me>
2019-10-30 19:14:10 +0100

    implemented version and build command cleaning up structure

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,32 +1,9 @@
 package bot
 
 import (
-	"errors"
 	"fmt"
 	"github.com/nlopes/slack"
-	"github.com/stgrmks/Rodelbahn-Tracker/internal/config"
-	"github.com/stgrmks/Rodelbahn-Tracker/internal/logger"
 	"strings"
-)
-
-var log = logger.Logger.WithField("package", "bot")
-
-const (
-	Equal        = "EQUAL"
-	Inequal      = "INEQUAL"
-	Bigger       = "BIGGER"
-	BiggerEqual  = "BIGGEREQUAL"
-	Smaller      = "SMALLER"
-	SmallerEqual = "SMALLEREQUAL"
-	All          = "ALL"
-)
-
-var (
-	VERSION           = "0.2.0"
-	BUILD             = "0.2.0"
-	KillBot           = make(chan bool)
-	KillPeriodicCrawl = make(chan bool)
-	MyConfig          config.Config
 )
 
 func StartBot() {
@@ -78,52 +55,18 @@ func StartBot() {
 
 }
 
-func msgSplit(cmdIsToSplits string, splits int, msg string, substr string) ([]string, error) {
-	cmdRaw := strings.Split(msg, substr)
-	cmpResult := false
-	switch cmdIsToSplits {
-
-	case Equal:
-		cmpResult = len(cmdRaw) == splits
-		break
-
-	case Inequal:
-		cmpResult = len(cmdRaw) != splits
-		break
-
-	case Bigger:
-		cmpResult = len(cmdRaw) > splits
-		break
-
-	case BiggerEqual:
-		cmpResult = len(cmdRaw) >= splits
-		break
-
-	case Smaller:
-		cmpResult = len(cmdRaw) < splits
-		break
-
-	case SmallerEqual:
-		cmpResult = len(cmdRaw) <= splits
-		break
-
-	case All:
-		cmpResult = true
-		break
-	}
-
-	if !cmpResult {
-		err := errors.New("unknown command")
-		log.Errorln("Comparison: ", cmpResult, "CommandRaw Len: ", len(cmdRaw), " Splits: ", splits, " CommandRaw: ", cmdRaw, " Error: ", err)
-		return cmdRaw, err
-	}
-	return cmdRaw, nil
-}
-
 func commandHandler(userId string, chanId string, msg string, rtm *slack.RTM) {
+	returnMsg := ""
 	cmdSplit, err := msgSplit(Bigger, 0, msg, " ")
 	if err != nil {
-		returnMsg := fmt.Sprintf("Sorry <@%s>, %s :(", userId, err.Error())
+		returnMsg = fmt.Sprintf("Sorry <@%s>, %s :(", userId, err.Error())
+		rtm.SendMessage(rtm.NewOutgoingMessage(returnMsg, chanId))
+	}
+	cmd := cmdSplit[1]
+	switch cmd {
+	case "version":
+		log.Print("Sending Version and Build Info")
+		returnMsg = fmt.Sprintf("<@%s> Version: %s Build: %s", userId, VERSION, BUILD)
 		rtm.SendMessage(rtm.NewOutgoingMessage(returnMsg, chanId))
 	}
 
