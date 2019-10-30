b9c164c5206cd52a73f5f8dae82a13f0469fd160
stgrmks <m.steger@pm.me>
2019-10-30 14:27:04 +0100

    restructuring

--- go.mod
+++ go.mod
@@ -6,9 +6,7 @@ require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/PuerkitoBio/goquery v1.5.0
 	github.com/fsnotify/fsnotify v1.4.7
-	github.com/robfig/cron v0.0.0-20180505203441-b41be1df6967
-	github.com/sbstjn/allot v0.0.0-20161025071122-1f2349af5ccd // indirect
-	github.com/sbstjn/hanu v0.1.0
+	github.com/nlopes/slack v0.6.0
 	github.com/sirupsen/logrus v1.4.1
 	github.com/spf13/viper v1.3.2
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"github.com/stgrmks/Rodelbahn-Tracker/internal/bot"
 	"github.com/stgrmks/Rodelbahn-Tracker/internal/logger"
-	"github.com/stgrmks/Rodelbahn-Tracker/internal/rbtracker"
 )
 
 var log = logger.Logger.WithField("package", "main")
@@ -12,13 +12,13 @@ func main() {
 	defer log.Info("Stopping bot go routine!")
 
 	// init stuff
-	rbtracker.MyConfig.Load("configs/config.json")
+	bot.MyConfig.Load("configs/config.json")
 
 	// start bot
 	log.Info("Starting bot in go routine...")
-	go rbtracker.StartBot()
+	go bot.StartBot()
 
 	// waiting for shutdown signal
-	<-rbtracker.KillBot
+	<-bot.KillBot
 
 }
